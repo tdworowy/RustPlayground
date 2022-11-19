4cb0dbf4b49f88309ea4080dc8bb93cb39c44b67
Tomasz Dworowy <dworowytomasz@gmail.com>
2022-11-19 18:57:01 +0100

    update

---
+++ exercises/exercise2_de_duplication/src/main.rs
@@ -0,0 +1,10 @@
+fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+    nums.dedup();
+    nums.len() as i32
+}
+
+fn main() {
+    let nums = &mut vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+    println!("{}", remove_duplicates(nums));
+    println!("{:?}", nums);
+}
