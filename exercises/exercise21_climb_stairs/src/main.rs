fe677bcc537e977a5c09a0ac9a4fde80815fe166
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-01-23 15:14:01 +0100

    update

--- exercises/exercise21_climb_stairs/src/main.rs
+++ exercises/exercise21_climb_stairs/src/main.rs
@@ -1,4 +1,6 @@
-fn climb_stairs(n: i32) -> i32 {
+use itertools::{Itertools, MultiProduct};
+
+fn climb_stairs(n: i128) -> i128 {
     let mut a = 0;
     let mut b = 1;
     for _ in 0..n {
@@ -7,9 +9,52 @@ fn climb_stairs(n: i32) -> i32 {
     b
 }
 
+pub fn product_repeat<I>(it: I, repeat: usize) -> MultiProduct<I>
+where
+    I: Iterator + Clone,
+    I::Item: Clone,
+{
+    std::iter::repeat(it).take(repeat).multi_cartesian_product()
+}
+pub trait ProductRepeat: Iterator + Clone
+where
+    Self::Item: Clone,
+{
+    fn product_repeat(self, repeat: usize) -> MultiProduct<Self> {
+        std::iter::repeat(self)
+            .take(repeat)
+            .multi_cartesian_product()
+    }
+}
+
+impl<T: Iterator + Clone> ProductRepeat for T where T::Item: Clone {}
+
+fn print_all_staris_combinations(n: usize) {
+    let mut results: Vec<Vec<usize>> = Vec::new();
+    for i in 2..n {
+        results.extend(product_repeat(1..3, i).collect::<Vec<Vec<usize>>>());
+    }
+    results.retain(|x| {
+        let s: usize = x.iter().sum();
+        s == n
+    });
+
+    let ones: Vec<usize> = vec![1; n];
+    results.push(ones);
+    results.into_iter().for_each(|x| {
+        println!("{:?}", x);
+    });
+}
+
 fn main() {
-    println!("{:?}", climb_stairs(2)); //2
-    println!("{:?}", climb_stairs(3)); //3
-    println!("{:?}", climb_stairs(4)); //5
-    println!("{:?}", climb_stairs(5)); //8
+    // println!("{:?}", climb_stairs(2)); //2
+    // println!("{:?}", climb_stairs(3)); //3
+    // println!("{:?}", climb_stairs(4)); //5
+    // println!("{:?}", climb_stairs(5)); //8
+    // println!("{:?}", climb_stairs(183)); //127127879743834334146972278486287885163
+
+    // print_all_staris_combinations(5);
+    // print_all_staris_combinations(8);
+
+    print_all_staris_combinations(50);
 }
